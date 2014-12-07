e3814a915a38c09bc6e2add5a0cec603df1b42c1
GUIHAL Maxime <sgp.jaconil@gmail.com>
2014-12-07 19:01:38 +0100

    Work in progress

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            options: {
+                banner: '/* <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            build: {
+                files: {
+                    'js/dist/main.min.js': ['js/src/*.js']
+                }
+            }
+        },
+
+        less: {
+            options: {
+                rootpath: '../'
+            },
+            build: {
+                files: {
+                    'css/src/main.css': ['css/src/less/main.less']
+                }
+            }
+        },
+
+        cssmin: {
+            options: {
+                banner: '/* <%= grunt.template.today("dd-mm-yyyy") %> */'
+            },
+            build: {
+                files: {
+                    'css/dist/main.min.css': ['css/src/*.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['uglify', 'less', 'cssmin']);
+};
---
+++ js/src/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    function addTransition() {
+        $(this).addClass('transition');
+    }
+
+    // Fondus
+    $('li a, nav a').hide();
+    $('#photo img').hide().fadeIn(2000, function() {
+        $(this).addClass('transition');
+
+        $('nav a').fadeIn(2800, addTransition);
+
+        $('li.github a').fadeIn(1200, addTransition);
+        $('li.linkedin a').fadeIn(1600, addTransition);
+        $('li.viadeo a').fadeIn(2000, addTransition);
+        $('li.socialnetworks a').fadeIn(2000, addTransition);
+        $('li.dyb a').fadeIn(2400, addTransition);
+        $('li.pdf a').fadeIn(2800, addTransition);
+        $('li.back a').fadeIn(2800, addTransition);
+    });
+
+    // Effet typewriter
+    var element = $('#about div');
+    var data = [];
+
+    element.text().trim().split('\n').every(function(element, index, array) {
+        data.push(element.trim());
+        data.push({text: '          ', element: '<span></span>'});
+        return index < array.length;
+    });
+
+    element.typewriter(data, 0, 0);
+
+    // Infobulles
+    $('header li a').tooltip({
+        placement: 'bottom'
+    });
+});
